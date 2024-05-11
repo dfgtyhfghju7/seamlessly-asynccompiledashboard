0e4f6362e444d1934397ea180f09bc561500b70a
dfgtyhfghju7 <gs1ganderic@hotmail.com>
2024-05-11 21:31:07 +0700

    hello

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function isValidBST(root) {
-  return isValid(root, null, null);
-}
-function isValid(node, min, max) {
-  if (!node) return true;
-  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
-    return false;
-  return (
-    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
-  );
+function connect(root) {
+  if (!root) return root;
+  let levelStart = root;
+  while (levelStart) {
+    let curr = levelStart;
+    while (curr) {
+      if (curr.left) curr.left.next = curr.right;
+      if (curr.right && curr.next) curr.right.next = curr.next.left;
+      curr = curr.next;
+    }
+    levelStart = levelStart.left;
+  }
+  return root;
 }
